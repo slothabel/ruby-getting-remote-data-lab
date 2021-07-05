cede4f58fd3e2acea16cc658f36b9b6de42f2400
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-04 23:53:54 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+        data.collect {|i| i } 
+    end
+    
+    
+    
+    
+end
+
